03e5d14ad0f8a85ae8b8ba7864ade757cf6987bd
aripandini <ariannepandini@hotmail.com>
2023-08-20 23:26:52 -0400

    LoginForm & SignupForm refactored

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,10 +1,15 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
+import { ADD_USER } from '../utils/mutations';
+import { useMutation } from '@apollo/client';
+
 
 const SignupForm = () => {
+
+  const [addUser] = useMutation(ADD_USER);
+
   // set initial form state
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
   // set state for form validation
@@ -27,16 +32,10 @@ const SignupForm = () => {
       event.stopPropagation();
     }
 
-    try {
-      const response = await createUser(userFormData);
+      try {
+        const { data } = await addUser({ variables: {...userFormData }});
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
@@ -47,7 +46,7 @@ const SignupForm = () => {
       email: '',
       password: '',
     });
-  };
+ };
 
   return (
     <>
