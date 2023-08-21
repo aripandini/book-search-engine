0954fb7cddafd8f37af6f339db8f4062c2070884
aripandini <ariannepandini@hotmail.com>
2023-08-20 23:05:20 -0400

    App.JS Apollo Provider created. SearchBooks.js & SavedBooks.js botk updated to use query and muations.

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -1,4 +1,11 @@
 import { useState, useEffect } from 'react';
+import { useMutation } from '@apollo/client';
+
+import Auth from '../utils/auth';
+import { searchGoogleBooks } from '../utils/API';
+import { SAVE_BOOK } from '../utils/mutations;'
+import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+
 import {
   Container,
   Col,
@@ -8,9 +15,6 @@ import {
   Row
 } from 'react-bootstrap';
 
-import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
-import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -20,6 +24,8 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+ // Mutation Hook
+  const [saveBook] = useMutation(SAVE_BOOK);
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
@@ -72,11 +78,10 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+       await saveBook({
+        variables: { bookData: { ...bookToSave }},
+      });
+      setSavedBookIds([...saveBookIds, bookToSave.bookId]);
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
